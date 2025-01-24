2397f20deca86143c7115d2e1d98777624a04164
70928manson <manson972@yahoo.com>
2025-01-24 11:22:57 +0800

    feat: add JobListings section and JobCard component

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,12 @@
 import Navbar from "@/components/Navbar.vue";
 import Hero from "@/components/Hero.vue";
 import HomeCards from "@/components/HomeCards.vue";
+import JobListings from "@/components/JobListings.vue";
 </script>
 
 <template>
   <Navbar />
   <Hero />
   <HomeCards />
-  <h1 class="text-2xl">Vue Jobs</h1>
+  <JobListings :limit="3" :showButton="true"/>
 </template>
