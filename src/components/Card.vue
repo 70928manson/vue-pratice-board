90ffa6d0b81e844008fd344ebbcc04a5812505a7
70928manson <manson972@yahoo.com>
2025-01-24 09:26:52 +0800

    feat: add HomeCards section and Card component

---
+++ src/components/Card.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { defineProps } from 'vue';
+
+defineProps({
+    bg: {
+        type: String,
+        default: 'bg-gray-100'
+    },
+    title: {
+        type: String,
+        default: 'title'
+    },
+    description: {
+        type: String,
+        default: 'description'
+    },
+    buttonBg: {
+        type: String,
+        default: 'bg-black'
+    },
+    buttonText: {
+        type: String,
+        default: 'button text'
+    }
+})
+</script>
+
+<template>
+    <div :class="`${bg} p-6 rounded-lg shadow-md`">
+        <h2 class="text-2xl font-bold">{{ title }}</h2>
+        <p class="mt-2 mb-4">
+            {{ description }}
+        </p>
+        <a href="jobs.html" :class="`inline-block ${buttonBg} text-white rounded-lg px-4 py-2 hover:bg-gray-700`">
+            {{ buttonText }}
+        </a>
+    </div>
+</template>
---
+++ src/components/HomeCards.vue
@@ -0,0 +1,41 @@
+<script setup>
+import Card from '@/components/Card.vue';
+
+const cardsData = [
+    {
+        id: 1,
+        bg: "bg-gray-100", 
+        title: "For Developers",
+        description: "Browse our Vue jobs and start your career today", 
+        buttonBg: "bg-black",
+        buttonText:"Browse Jobs" 
+    },
+    {
+        id: 2,
+        bg: "bg-green-100",
+        title: "For Employers",
+        description: "List your job to find the perfect developer for the role", 
+        buttonBg: "bg-green-500",
+        buttonText: "Add Job",
+    }
+]
+
+</script>
+
+<template>
+    <section class="py-4">
+        <div class="container-xl lg:container m-auto">
+            <div class="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
+                <Card 
+                    v-for="data in cardsData"
+                    :key="data.id"
+                    :bg="data.bg"
+                    :title="data.title"
+                    :description="data.description"
+                    :buttonBg="data.buttonBg"
+                    :buttonText="data.buttonText"
+                />
+            </div>
+        </div>
+    </section>
+</template>
