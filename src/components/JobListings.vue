50d6dcec891ecbe237fcc53752c7a39c4902972d
70928manson <manson972@yahoo.com>
2025-02-05 15:36:43 +0800

    feat: JobCard 新增查看更多or隱藏部份 description 功能, location 加上 map marker icon

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -26,8 +26,9 @@ defineProps({
         </div>
     </section>
     <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
-        <a href="jobs.html" class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700">View
-            All Jobs</a>
+        <a href="/jobs" class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700">
+            View All Jobs
+        </a>
     </section>
 
 </template>
